332dea4225121330b9550fd04c442cd95014336e
David Gassner <david.gassner@gmail.com>
2021-06-03 09:24:34 -0700

    Updates for Arctic Fox beta 3

--- app/src/main/java/com/example/scopedstoragetest/MainActivity.kt
+++ app/src/main/java/com/example/scopedstoragetest/MainActivity.kt
@@ -8,7 +8,8 @@ import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
 import com.example.scopedstoragetest.databinding.ActivityMainBinding
@@ -16,9 +17,13 @@ import com.example.scopedstoragetest.utilities.FileUtils
 
 class MainActivity : AppCompatActivity() {
 
+    // Persistent directory reference
     private var appStorageDir: DocumentFile? = null
+
+    // View bindings
     private lateinit var binding: ActivityMainBinding
 
+    // Get permission to work with a directory in external storage
     private val storageDirRequest =
         registerForActivityResult(StartActivityForResult()) { result: ActivityResult ->
 
@@ -30,10 +35,6 @@ class MainActivity : AppCompatActivity() {
 
             appStorageDir = DocumentFile.fromTreeUri(applicationContext, uri)
 
-//          debug code
-//            Log.d(LOG_TAG, "Selected Uri: $uri")
-//            Log.d(LOG_TAG, "Directory name: ${getFileName(directory!!)}")
-
             val fileList = appStorageDir?.listFiles() ?: emptyArray()
 
             for (docFile in fileList) {
@@ -42,10 +43,10 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    // Import a single file through SAF
     private val importFileRequest =
-        registerForActivityResult(StartActivityForResult()) {
+        registerForActivityResult(GetContent()) { sourceUri ->
 
-            val sourceUri = it.data?.data ?: return@registerForActivityResult
             contentResolver.openInputStream(sourceUri).use { input ->
 
                 Log.d(LOG_TAG, "URI: $sourceUri")
@@ -71,6 +72,7 @@ class MainActivity : AppCompatActivity() {
             Log.d(LOG_TAG, "End copy")
         }
 
+    // Set up the user interface and permissions
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -82,14 +84,18 @@ class MainActivity : AppCompatActivity() {
             createFileButton.setOnClickListener {
                 FileUtils.makeFile(contentResolver, appStorageDir, "myFile.txt")
             }
-            createDirButton.setOnClickListener { FileUtils.createDirectory(appStorageDir, "newDirectory") }
+            createDirButton.setOnClickListener {
+                FileUtils.createDirectory(
+                    appStorageDir,
+                    "newDirectory"
+                )
+            }
             importFileButton.setOnClickListener { requestImportFile() }
         }
 
+        // Get existing permissions and reference to the app storage directory
         val permissions = contentResolver.persistedUriPermissions
         if (permissions.isNotEmpty()) {
-//            Log.d(LOG_TAG, "Is read? ${permissions[0].isReadPermission}")
-//            Log.d(LOG_TAG, "Is write? ${permissions[0].isWritePermission}")
             val storageUri = permissions[0].uri
             appStorageDir = DocumentFile.fromTreeUri(this, storageUri)
         }
@@ -98,23 +104,11 @@ class MainActivity : AppCompatActivity() {
 
     // Start a request to import a file from SAF
     private fun requestImportFile() {
-        Intent(Intent.ACTION_OPEN_DOCUMENT)
-            .addCategory(Intent.CATEGORY_OPENABLE)
-            .setType("audio/*").also {
-                importFileRequest.launch(it)
-            }
+        importFileRequest.launch("audio/*")
     }
 
     // Let the user select a directory
     private fun requestStorageDirectory() {
-
-//        val storageManager = getSystemService(StorageManager::class.java)
-//        val volumes = storageManager.storageVolumes
-//        for (volume in volumes) {
-//            Log.i(LOG_TAG, "$volume, removable=${volume.isRemovable}")
-//        }
-
-        // Create and launch the intent
         Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).also {
             it.addFlags(
                 Intent.FLAG_GRANT_READ_URI_PERMISSION or
